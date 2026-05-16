fec058dcca8b0b7485c6b481b9a624337f4fc30e
mushrafmim <mushrafmim@gmail.com>
2026-05-16 20:52:18 +0530

    refactor: move setNestedKey function to utils.go for better organization

---
+++ taskflow/orchestrator/utils.go
@@ -0,0 +1,26 @@
+package orchestrator
+
+// setNestedKey sets a value in a map using a dot-separated path.
+func setNestedKey(m map[string]any, dotPath string, value any) {
+	if dotPath == "" {
+		return
+	}
+	for i := 0; i < len(dotPath); i++ {
+		if dotPath[i] == '.' {
+			key := dotPath[:i]
+			rest := dotPath[i+1:]
+			sub, ok := m[key]
+			if !ok || sub == nil {
+				sub = make(map[string]any)
+			}
+			subMap, ok := sub.(map[string]any)
+			if !ok {
+				subMap = make(map[string]any)
+			}
+			setNestedKey(subMap, rest, value)
+			m[key] = subMap
+			return
+		}
+	}
+	m[dotPath] = value
+}
